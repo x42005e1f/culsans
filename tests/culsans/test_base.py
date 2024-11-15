d7884aa41fd4c338a2164e0f5219e7a4d06965dd
Ilya Egorov <0x42005e1f@gmail.com>
2024-11-16 03:55:41 +0400

    test: generalize tests

--- tests/culsans/test_base.py
+++ tests/culsans/test_base.py
@@ -17,12 +17,23 @@ class _TestQueueBase:
         assert queue.async_q.wrapped is queue
 
         assert queue.unfinished_tasks == 0
+        assert queue.sync_q.unfinished_tasks == 0
+        assert queue.async_q.unfinished_tasks == 0
+
         assert not queue.is_shutdown
+        assert not queue.sync_q.is_shutdown
+        assert not queue.async_q.is_shutdown
 
         assert queue.maxsize == 0
+        assert queue.sync_q.maxsize == 0
+        assert queue.async_q.maxsize == 0
 
         with pytest.raises(AttributeError):
             queue.nonexistent_attribute
+        with pytest.raises(AttributeError):
+            queue.sync_q.nonexistent_attribute
+        with pytest.raises(AttributeError):
+            queue.async_q.nonexistent_attribute
 
     def test_setters(self):
         queue = self.factory()
@@ -35,15 +46,38 @@ class _TestQueueBase:
         with pytest.raises(AttributeError):
             queue.unfinished_tasks = queue.unfinished_tasks
         with pytest.raises(AttributeError):
+            queue.sync_q.unfinished_tasks = queue.sync_q.unfinished_tasks
+        with pytest.raises(AttributeError):
+            queue.async_q.unfinished_tasks = queue.async_q.unfinished_tasks
+
+        with pytest.raises(AttributeError):
             queue.is_shutdown = queue.is_shutdown
+        with pytest.raises(AttributeError):
+            queue.sync_q.is_shutdown = queue.sync_q.is_shutdown
+        with pytest.raises(AttributeError):
+            queue.async_q.is_shutdown = queue.async_q.is_shutdown
 
         queue.maxsize = 1
         assert queue.maxsize == 1
         queue.maxsize = -1
         assert queue.maxsize == -1
 
+        queue.sync_q.maxsize = 1
+        assert queue.maxsize == 1
+        queue.sync_q.maxsize = -1
+        assert queue.maxsize == -1
+
+        queue.async_q.maxsize = 1
+        assert queue.maxsize == 1
+        queue.async_q.maxsize = -1
+        assert queue.maxsize == -1
+
         with pytest.raises(AttributeError):
             queue.nonexistent_attribute = 42
+        with pytest.raises(AttributeError):
+            queue.sync_q.nonexistent_attribute = 42
+        with pytest.raises(AttributeError):
+            queue.async_q.nonexistent_attribute = 42
 
     def test_deleters(self):
         queue = self.factory()
@@ -56,13 +90,30 @@ class _TestQueueBase:
         with pytest.raises(AttributeError):
             del queue.unfinished_tasks
         with pytest.raises(AttributeError):
+            del queue.sync_q.unfinished_tasks
+        with pytest.raises(AttributeError):
+            del queue.async_q.unfinished_tasks
+
+        with pytest.raises(AttributeError):
             del queue.is_shutdown
+        with pytest.raises(AttributeError):
+            del queue.sync_q.is_shutdown
+        with pytest.raises(AttributeError):
+            del queue.async_q.is_shutdown
 
         with pytest.raises(AttributeError):
             del queue.maxsize
+        with pytest.raises(AttributeError):
+            del queue.sync_q.maxsize
+        with pytest.raises(AttributeError):
+            del queue.async_q.maxsize
 
         with pytest.raises(AttributeError):
             del queue.nonexistent_attribute
+        with pytest.raises(AttributeError):
+            del queue.sync_q.nonexistent_attribute
+        with pytest.raises(AttributeError):
+            del queue.async_q.nonexistent_attribute
 
     def test_unfinished(self):
         queue = self.factory()
